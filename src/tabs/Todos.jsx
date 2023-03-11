a03708794d1811dc7bbb35ba041f77ec0908bd95
Zub04ek <nastasyz@ukr.net>
2023-03-11 16:57:56 +0200

    add 2nd completed task

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,95 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+    isEditing: false,
+    currentTodo: {},
+  };
+
+  componentDidMount() {
+    const savedTodos = JSON.parse(localStorage.getItem('todos'));
+    if (savedTodos) {
+      this.setState({ todos: savedTodos });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { todos } = this.state;
+    if (prevState.todos !== todos) {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+  }
+
+  handleOnSubmit = value => {
+    const todo = {
+      id: nanoid(),
+      text: value,
+    };
+    this.setState(({ todos }) => ({ todos: [...todos, todo] }));
+  };
+
+  deleteTodo = todoId => {
+    this.setState(({ todos }) => ({
+      todos: todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  handleEdit = todo => {
+    this.setState({ isEditing: true, currentTodo: { ...todo } });
+  };
+
+  handleCancel = () => {
+    this.setState({ isEditing: false });
+  };
+
+  handleInputEditChange = event => {
+    const { currentTodo } = this.state;
+    this.setState({
+      currentTodo: { ...currentTodo, text: event.currentTarget.value },
+    });
+  };
+
+  handleFormUpdate = event => {
+    event.preventDefault();
+    const { currentTodo, todos } = this.state;
+    if (currentTodo.text === '') {
+      return alert('Enter message!');
+    }
+    const change = todos.map(todo => {
+      return todo.id === currentTodo.id ? currentTodo : todo;
+    });
+    this.setState({ todos: change, isEditing: false });
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    const { isEditing, currentTodo } = this.state;
+    return (
+      <>
+        {isEditing ? (
+          <EditForm
+            currentTodo={currentTodo}
+            onCancel={this.handleCancel}
+            onChange={this.handleInputEditChange}
+            onUpdate={this.handleFormUpdate}
+          />
+        ) : (
+          <SearchForm onSubmit={this.handleOnSubmit} />
+        )}
+        <Grid>
+          {this.state.todos.map(({ id, text }, index) => (
+            <GridItem key={id}>
+              <Todo
+                id={id}
+                text={text}
+                index={index + 1}
+                onDelete={this.deleteTodo}
+                onEdit={() => this.handleEdit({ id, text })}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </>
+    );
   }
 }
